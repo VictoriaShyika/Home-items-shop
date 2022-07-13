5665900d7a0ba49f56a51392222049d0f2dbd205
VictoriaShyika <victoriashyika@gmail.com>
2022-07-13 17:41:23 +0300

    ShoppingCart: Implemented adding items in cart

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import Items from "./components/Items";
 import { itemsList } from "./items-list";
 
 function App() {
-  const [items, setItems] = useState(itemsList)
+  const [items, setItems] = useState(itemsList);
+  const [orders, setOrders] = useState([]);
+
+  const addToOrders = (item) => {
+    let isInArray = false;
+    orders.forEach((el) => {
+      if (el.id === item.id) {
+        isInArray = true;
+      }
+    });
+
+    if (!isInArray) {
+      setOrders([...orders, item]);
+    }
+  };
+
   return (
     <div className="wrapper">
-      <Header />
-      <Items items={items} />
+      <Header orders={orders} />
+      <Items items={items} onAdd={addToOrders} />
       <Footer />
     </div>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Item(props) {
   return (
     <div className="item">
-      <img src={"./img/" + props.item.img} />
+      <img src={"./img/" + props.item.img} alt={props.item.desc} />
       <h2>{props.item.title}</h2>
       <p>{props.item.desc}</p>
       <b>{props.item.price}$</b>
-      <div className="add-to-cart">+</div>
+      <div className="add-to-cart" onClick={() => props.onAdd(props.item)}>+</div>
     </div>
   );
 }
